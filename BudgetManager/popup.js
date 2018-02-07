73cbcfbc6f0f5600e9788fe8566e69804aa30148
PoojaB26 <pbhaumik26@gmail.com>
2018-02-08 01:26:36 +0530

    Added options and notifications

--- BudgetManager/popup.js
+++ BudgetManager/popup.js
@@ -1,12 +1,13 @@
 $(function () {
 
-    chrome.storage.sync.get('total', function (budget) {
+    chrome.storage.sync.get(['total', 'limit'], function (budget) {
         $('#total').text(budget.total);
+        $('#limit').text(budget.limit);
 
     })
 
     $('#spendAmount').click(function () {
-        chrome.storage.sync.get('total', function (budget){
+        chrome.storage.sync.get(['total', 'limit'], function (budget){
             var newTotal = 0;
             if(budget.total){
                 newTotal += parseInt(budget.total);
@@ -19,7 +20,18 @@ $(function () {
 
             chrome.storage.sync.set({
                 'total': newTotal
-            })
+            }, function () {
+                if(amount && newTotal>=budget.limit){
+                    var notifOptions = {
+                        type: 'basic',
+                        iconUrl: 'icon48.png',
+                        title : 'Limit reached',
+                        message : "Reached limit"
+
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                }
+            });
 
             $('#total').text(newTotal);
             $('#amount').val('');
